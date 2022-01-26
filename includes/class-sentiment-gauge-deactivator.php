186a005cf5f34328581c9c872a54c73bf077a60e
Richard Peck <rpeck@frontlineutilities.co.uk>
2022-01-26 16:41:36 +0000

    Initial

---
+++ includes/class-sentiment-gauge-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.github.com/richpeck
+ * @since      1.0.0
+ *
+ * @package    Sentiment_Gauge
+ * @subpackage Sentiment_Gauge/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Sentiment_Gauge
+ * @subpackage Sentiment_Gauge/includes
+ * @author     Richard Peck <rpeck@frontlineutilities.co.uk>
+ */
+class Sentiment_Gauge_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
