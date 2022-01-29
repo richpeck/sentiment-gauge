9d4a98c0c4c618111e6324f11431c44c14423768
Richard Peck <rpeck@frontlineutilities.co.uk>
2022-01-29 10:00:19 +0000

    Update sentiment-gauge-public-display.php

--- public/partials/sentiment-gauge-public-display.php
+++ public/partials/sentiment-gauge-public-display.php
@@ -19,8 +19,8 @@
     $title = $a['title'] == "" ? '&nbsp;' : '<div class="title">' . $a['title'] . '</div>';
 ?>
 
-<div class="gauge <?php esc_html_e( $a['align'], 'sentiment-gauge' ); ?>" style="--percent: <?php esc_html_e( $a['percent'], 'sentiment-gauge' ); ?>; --color: <?php esc_html_e( $a['color'], 'sentiment-gauge' ); ?>; --margin: <?php esc_html_e( $a['margin'], 'sentiment-gauge' ); ?>; --max-width: <?php esc_html_e( $a['max-width'], 'sentiment-gauge' ); ?>; --background-color: <?php esc_html_e( $a['background-color'], 'sentiment-gauge' ); ?>; --font-color: <?php esc_html_e( $a['font-color'], 'sentiment-gauge' ); ?>; --border-color: <?php esc_html_e( $a['border-color'], 'sentiment-gauge' ); ?>;">
-    <div class="element" data-percent="<?php esc_html_e( $a['percent'], 'sentiment-gauge' ); ?>">
-        <? esc_html_e( $title, 'sentiment-gauge' ); ?>
+<div class="gauge <?php esc_attr_e( $a['align'], 'sentiment-gauge' ); ?>" style="--percent: <?php esc_attr_e( $a['percent'], 'sentiment-gauge' ); ?>; --color: <?php esc_attr_e( $a['color'], 'sentiment-gauge' ); ?>; --margin: <?php esc_attr_e( $a['margin'], 'sentiment-gauge' ); ?>; --max-width: <?php esc_attr_e( $a['max-width'], 'sentiment-gauge' ); ?>; --background-color: <?php esc_attr_e( $a['background-color'], 'sentiment-gauge' ); ?>; --font-color: <?php esc_attr_e( $a['font-color'], 'sentiment-gauge' ); ?>; --border-color: <?php esc_attr_e( $a['border-color'], 'sentiment-gauge' ); ?>;">
+    <div class="element" data-percent="<?php esc_attr_e( $a['percent'], 'sentiment-gauge' ); ?>">
+        <? _e( $title, 'sentiment-gauge' ); ?>
     </div>
 </div>
