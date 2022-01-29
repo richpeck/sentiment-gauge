b740540973020faaa740a34cd483d82f4ceb514c
Richard Peck <rpeck@frontlineutilities.co.uk>
2022-01-29 09:51:58 +0000

    Update sentiment-gauge-public-display.php

--- public/partials/sentiment-gauge-public-display.php
+++ public/partials/sentiment-gauge-public-display.php
@@ -19,7 +19,7 @@
     $title = $a['title'] == "" ? '&nbsp;' : '<div class="title">' . $a['title'] . '</div>';
 ?>
 
-<div class="gauge <?php echo $a['align']; ?>" style="--percent: <?php echo $a['percent']; ?>; --color: <?php echo $a['color']; ?>; --margin: <?php echo $a['margin']; ?>; --max-width: <?php echo $a['max-width']; ?>; --background-color: <?php echo $a['background-color']; ?>; --font-color: <?php echo $a['font-color']; ?>; --border-color: <?php echo $a['border-color']; ?>;">
+<div class="gauge <?php esc_html_e( $a['align'], 'sentiment-gauge' ); ?>" style="--percent: <?php esc_html_e( $a['percent'], 'sentiment-gauge' ); ?>; --color: <?php esc_html_e( $a['color'], 'sentiment-gauge' ); ?>; --margin: <?php esc_html_e( $a['margin'], 'sentiment-gauge' ); ?>; --max-width: <?php echo $a['max-width']; ?>; --background-color: <?php echo $a['background-color']; ?>; --font-color: <?php echo $a['font-color']; ?>; --border-color: <?php echo $a['border-color']; ?>;">
     <div class="element" data-percent="<?php echo $a['percent']; ?>">
         <? echo $title; ?>
     </div>
